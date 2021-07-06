356e277e57a639bad68136dd0ab0b83282cb20bf
bin.he <bin.he@Leginvr.com>
2021-07-06 15:38:57 +0800

    fix a fault with no input by caps not compatible

--- gst-director/c/gstdc.c
+++ gst-director/c/gstdc.c
@@ -500,7 +500,7 @@ gchar * message_process (const gchar * msg)
     int a, v, rs=0;
     char cur_v[100],cur_a[100];
     GstcStatus r;
-    sleep(1);
+    sleep(0.8);
     g_print ("switch \n");
     sprintf (pd->pipename, "%s", "preview");
     if (is_exist(pd->pipename)) {

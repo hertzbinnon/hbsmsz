997c098246a81d7f457a0a73d55f603877cc27c3
hertz <bin.he@letinvr.com>
2021-11-18 22:33:10 -0500

    fix compile

--- gst-director/c/env.sh
+++ gst-director/c/env.sh
@@ -1,2 +1,4 @@
-export PKG_CONFIG_PATH=/home/hertz/sdb1/hbsmsz/Install/lib/x86_64-linux-gnu/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig
+#export PKG_CONFIG_PATH=/home/hertz/sdb1/hbsmsz/Install/lib/x86_64-linux-gnu/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig
+export PKG_CONFIG_PATH=/home/hertz/sdb1/cerbero/build/dist/linux_x86_64/lib/pkgconfig:/home/hertz/sdb1/cerbero/build/dist/linux_x86_64/lib/x86_64-linux-gnu/pkgconfig/:/usr/lib/x86_64-linux-gnu/pkgconfig
+
 
